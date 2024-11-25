a7810b28585f276451676d7b842f4bc651fa216d
hamza <i230510@isb.nu.edu.pk>
2024-11-26 00:59:59 +0500

    Initial COmmit

---
+++ GitLite.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hamza on 11/26/2024.
+//
+
+#include "GitLite.h"
---
+++ GitLite.h
@@ -0,0 +1,27 @@
+//
+// Created by hamza on 11/26/2024.
+//
+
+#ifndef GITLITE_H
+#define GITLITE_H
+
+
+
+class GitLite {
+
+public:
+    void welcome();
+    void init_menu();
+    void git_menu();
+    void git_init();
+    void git_add();
+    void git_commit();
+    void git_log();
+    void git_checkout();
+    void git_reset();
+
+};
+
+
+
+#endif //GITLITE_H
