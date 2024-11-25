c4ae46b2f326cc7d1e958fc8b0f58c8b2465cc64
hamza <i230510@isb.nu.edu.pk>
2024-11-26 01:24:56 +0500

    Added some boilderplate

---
+++ GLOBALS.h
@@ -0,0 +1,21 @@
+//
+// Created by hamza on 11/26/2024.
+//
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+enum tree_type {
+    AVL,
+    RBT,
+    BT
+};
+
+enum color {
+    RED,
+    BLACK
+};
+
+
+
+#endif //GLOBALS_H
