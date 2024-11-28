112f3a25079988972bd457513d6606c8a41cd476
hamza <i230510@isb.nu.edu.pk>
2024-11-28 17:14:12 +0500

    add - list structure

---
+++ STL_STRUCTURES/MyList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hamza on 11/28/2024.
+//
+
+#include "MyList.h"
---
+++ STL_STRUCTURES/MyList.h
@@ -0,0 +1,16 @@
+//
+// Created by hamza on 11/28/2024.
+//
+
+#ifndef MYLIST_H
+#define MYLIST_H
+
+
+
+class MyList {
+
+};
+
+
+
+#endif //MYLIST_H
