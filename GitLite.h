754b73822d875304009effccde2f4a9726fc2c51
hamza <i230510@isb.nu.edu.pk>
2024-11-29 16:32:28 +0500

    implement - menu

--- GitLite.h
+++ GitLite.h
@@ -9,14 +9,32 @@
 #include <filesystem>
 
 #include "csv_meta.h"
+#include "GLOBALS.h"
 
 using std::filesystem::path;
 
 using std::endl;
 using std::cout;
+using std::cin;
+
+
+
 
 class GitLite {
-    CSV_META csv_meta;
+    struct {
+        // pointer to tree
+        path root_path{};
+        int btree_order{};
+    }tree_info;
+    struct {
+        MyList<MyString> branches;
+        MyString current_branch;
+    } git_info;
+    struct {
+        CSV_META csv_meta;
+        int tree = tree_type::AVL;
+        int hash{};
+    } structure_info;
 	MyString title_str = "   ________.__  __  .____    .__  __          \n  /  _____/|__|/  |_|    |   |__|/  |_  ____ \n /   \\  ___|  \\   __\\    |   |  \\   __\\/ __ \\ \n \\    \\_\\  \\  ||  | |    |___|  ||  | \\  ___/ \n  \\______  /__||__| |_______ \\__||__|  \\___  > \n         \\/                 \\/             \\/";
 
 public:
